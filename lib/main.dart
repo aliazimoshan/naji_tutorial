63cd97ffefd5601c8d5655036c3bd9e42fef54d8
Aliazimoshan <a.azimoshan@gmail.com>
2022-12-20 09:48:39 +0330

    chnage title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NRDC',
+      title: 'NRDC Tutorial',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
